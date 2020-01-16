3b0275ab72bf3b2344f2feeae225adcfde941ff4
aleksandar <amarkovic29@gmail.com>
2020-01-16 22:05:50 +0100

    Add project files.

---
+++ QuatesApp/QuatesApp/PageModels/WelcomePageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace QuatesApp.PageModels
+{
+    public class WelcomePageModel
+    {
+        public Command SignInCommand { get; set; }
+
+        public WelcomePageModel()
+        {
+            SignInCommand = new Command(SignIn);    
+        }
+
+        private void SignIn()
+        {
+          
+        }
+    }
+}
---
+++ QuatesApp/QuatesApp/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using QuatesApp.PageModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuatesApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+            BindingContext = new WelcomePageModel();
+        }
+    }
+}
